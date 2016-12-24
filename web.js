ad09d60ef85d3df484335874f302f0701d3f3eb7
laotianstallion <rudy@zipnosis.com>
2016-12-24 09:22:40 -0600

    Fixed node configs

--- web.js
+++ web.js
@@ -1,7 +1,15 @@
-var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
+var express = require('express');
+var app = express();
 
-  app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 5000);
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/app'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
